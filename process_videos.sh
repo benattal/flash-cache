2b59ebdb5446917d946658204c10dabbe417b35f
breuckelen <attal.benjamin@gmail.com>
2024-09-04 23:53:08 +0000

    Update video backgrounds

--- process_videos.sh
+++ process_videos.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
 # Directory containing the video files
-VIDEO_DIR="videos/"
+VIDEO_DIR="supp/videos/"
 
 # Temporary output file
 TEMP_OUTPUT="temp_output.mp4"
 
+# # Iterate over each file in the directory that ends with _normals.mp4
+# for input_video in "$VIDEO_DIR"/*_normals.mp4; do
+#     # Generate the corresponding mask video path by replacing "normals" with "albedo"
+#     mask_video="${input_video/_normals.mp4/_relight_1.mp4}"
+
+#     # Check if the mask video exists
+#     if [[ -f "$mask_video" ]]; then
+#         # Run the process_videos.py script
+#         python process_videos.py "$input_video" "$mask_video" "$TEMP_OUTPUT"
+
+#         # Replace the original video with the processed output
+#         mv "$TEMP_OUTPUT" "$input_video"
+#     else
+#         echo "Mask video not found for $input_video"
+#     fi
+# done
+
 # Iterate over each file in the directory that ends with _normals.mp4
-for input_video in "$VIDEO_DIR"/*_normals.mp4; do
-    # Generate the corresponding mask video path
-    mask_video="${input_video/_normals.mp4/_mask.mp4}"
+for input_video in "$VIDEO_DIR"/*_albedo.mp4; do
+    # Generate the corresponding mask video path by replacing "normals" with "albedo"
+    mask_video="${input_video/_albedo.mp4/_relight_1.mp4}"
+
+    # Check if the mask video exists
+    if [[ -f "$mask_video" ]]; then
+        # Run the process_videos.py script
+        python process_videos.py "$input_video" "$mask_video" "$TEMP_OUTPUT"
+
+        # Replace the original video with the processed output
+        mv "$TEMP_OUTPUT" "$input_video"
+    else
+        echo "Mask video not found for $input_video"
+    fi
+done
+
+# Iterate over each file in the directory that ends with _normals.mp4
+for input_video in "$VIDEO_DIR"/*_roughness.mp4; do
+    # Generate the corresponding mask video path by replacing "normals" with "roughness"
+    mask_video="${input_video/_roughness.mp4/_relight_1.mp4}"
 
     # Check if the mask video exists
     if [[ -f "$mask_video" ]]; then
