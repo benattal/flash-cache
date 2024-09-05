7336642166dbc5e4ef8c5be1de391ac9c8ba26ab
breuckelen <attal.benjamin@gmail.com>
2024-09-05 02:02:53 +0000

    Update hotdog again

--- process_videos.sh
+++ process_videos.sh
@@ -6,27 +6,10 @@ VIDEO_DIR="supp/videos/"
 # Temporary output file
 TEMP_OUTPUT="temp_output.mp4"
 
-# # Iterate over each file in the directory that ends with _normals.mp4
-# for input_video in "$VIDEO_DIR"/*_normals.mp4; do
-#     # Generate the corresponding mask video path by replacing "normals" with "albedo"
-#     mask_video="${input_video/_normals.mp4/_relight_1.mp4}"
-
-#     # Check if the mask video exists
-#     if [[ -f "$mask_video" ]]; then
-#         # Run the process_videos.py script
-#         python process_videos.py "$input_video" "$mask_video" "$TEMP_OUTPUT"
-
-#         # Replace the original video with the processed output
-#         mv "$TEMP_OUTPUT" "$input_video"
-#     else
-#         echo "Mask video not found for $input_video"
-#     fi
-# done
-
 # Iterate over each file in the directory that ends with _normals.mp4
-for input_video in "$VIDEO_DIR"/*_albedo.mp4; do
+for input_video in "$VIDEO_DIR"/hotdog*_normals.mp4; do
     # Generate the corresponding mask video path by replacing "normals" with "albedo"
-    mask_video="${input_video/_albedo.mp4/_relight_1.mp4}"
+    mask_video="${input_video/_normals.mp4/_relight_2.mp4}"
 
     # Check if the mask video exists
     if [[ -f "$mask_video" ]]; then
@@ -41,9 +24,26 @@ for input_video in "$VIDEO_DIR"/*_albedo.mp4; do
 done
 
 # Iterate over each file in the directory that ends with _normals.mp4
-for input_video in "$VIDEO_DIR"/*_roughness.mp4; do
+for input_video in "$VIDEO_DIR"/hotdog*_albedo.mp4; do
+    # Generate the corresponding mask video path by replacing "normals" with "albedo"
+    mask_video="${input_video/_albedo.mp4/_relight_2.mp4}"
+
+    # Check if the mask video exists
+    if [[ -f "$mask_video" ]]; then
+        # Run the process_videos.py script
+        python process_videos.py "$input_video" "$mask_video" "$TEMP_OUTPUT"
+
+        # Replace the original video with the processed output
+        mv "$TEMP_OUTPUT" "$input_video"
+    else
+        echo "Mask video not found for $input_video"
+    fi
+done
+
+# Iterate over each file in the directory that ends with _normals.mp4
+for input_video in "$VIDEO_DIR"/hotdog*_roughness.mp4; do
     # Generate the corresponding mask video path by replacing "normals" with "roughness"
-    mask_video="${input_video/_roughness.mp4/_relight_1.mp4}"
+    mask_video="${input_video/_roughness.mp4/_relight_2.mp4}"
 
     # Check if the mask video exists
     if [[ -f "$mask_video" ]]; then
